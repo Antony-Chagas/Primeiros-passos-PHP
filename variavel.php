e46c679bef2c805488590d0a13b98273fa41964f
Antony Chagas <antony.tiagor445@gmail.com>
2022-10-31 19:43:15 -0300

    Primeiros passos com PHP

---
+++ variavel.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+$pi = 3.14159;
+$raio = fgets(STDIN);
+
+$area = $pi * pow($raio, 2);
+
+echo "A=" . number_format($area, 4, '.', '') . PHP_EOL;
+
+
+
+$valor = fgets(STDIN);
+$valor1 = fgets(STDIN);
+
+$valorTotal = $valor * $valor1;
+
+echo "PROD = $valorTotal" . PHP_EOL;
+*/
+
+$nota1 = fgets(STDIN);
+$nota2 = fgets(STDIN);
+
+$soma = $nota1 + $nota2;
+
+$maedia = $soma / 2;
+
+echo "MEDIA = " . $maedia . PHP_EOL;
+
+
+?>
+ 
+
+
